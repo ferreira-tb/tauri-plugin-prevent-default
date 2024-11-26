48280e59010b893244581f75c41c01f2c4ee9e11
Andrew Ferreira <andrew.shien2@gmail.com>
2024-11-26 10:48:00 -0300

    docs: use `include_str` macro

--- build.rs
+++ build.rs
@@ -2,14 +2,4 @@ const COMMANDS: &[&str] = &["keyboard", "pointer"];
 
 fn main() {
   tauri_plugin::Builder::new(COMMANDS).build();
-
-  /*let readme = std::fs::read_to_string("README.md").unwrap();
-  let lines = readme
-    .lines()
-    .map(|line| format!("//! {line}"))
-    .collect::<Vec<_>>()
-    .join("\n");
-
-  let src = std::fs::read_to_string("src/lib.rs").unwrap();
-  std::fs::write("src/lib.rs", format!("{lines}\n{src}")).unwrap();*/
 }
